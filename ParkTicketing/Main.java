412e651218d6ce510289c2f6be5fcafff2f6824a
Jaeyeon Ahn <jaeahn91@gmail.com>
2021-04-15 13:15:34 +0900

    Changes made: - OutputClass : created new methods "startofReceipt", "receipt", "receiptTotal" and "endofReceipt", which all combine in the Main Class in order to print purchase results in the form of receipt on the console.
    - Main : now using while(true){} structure instead of do {} while() to perform ticketing tasks continuously if requested. Now refering to several methods in OutputClass in order to print purchase results at the end of transaction.

--- ParkTicketing/Main.java
+++ ParkTicketing/Main.java
@@ -3,41 +3,60 @@ package ParkTicketing;
 import java.util.Scanner;
 
 public class Main {
-
-	public static int nextRound;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		int type = 0;
-		String idNumber;
-		int tickets = 0;
-		int option = 0;
+		int numTicket = 0;
+		int prefType = 0;
 		int age = 0;
 		int fee = 0;
 		int dcFee = 0;
-		do {
+		int totalfee = 0;
+		int nextRound = 0;
+		int exitproceed = 0;
+		String idNumber;
+		String ageType;
+		String transaction = "";
+		
 		InputClass inputclass = new InputClass();
-		
 		RunCalculator run = new RunCalculator();
-		type = inputclass.ticketType();
-		idNumber = inputclass.numID();
-		tickets = inputclass.numTickets();
-		option = inputclass.prefOptions();
-		
-		age = run.ageCal(idNumber);
-		fee = run.feeCal(type, age);
-		dcFee = run.prefCal(fee, option);
-		dcFee = tickets * dcFee;
-		
 		OutputClass outclass = new OutputClass();
-		outclass.printPrice(dcFee);
 		
-		
-		
-		nextRound = outclass.nextRound();
-		} while (nextRound == 1);
-		
+		while(true) {
+	
+			type = inputclass.ticketType();
+			idNumber = inputclass.numID();
+			numTicket = inputclass.numTickets();
+			prefType = inputclass.prefOptions();
+			
+			age = run.ageCal(idNumber);
+			fee = run.feeCal(type, age);
+			dcFee = run.prefCal(fee, prefType);
+			dcFee = numTicket * dcFee;
+			ageType = outclass.ageType(age);
+	
+			outclass.printPrice(dcFee);
+			
+			totalfee += dcFee;
+			transaction += outclass.receipt(type, ageType, numTicket,fee, prefType);
+			
+			nextRound = outclass.nextRound();
+			
+			if (nextRound == 2) {
+				outclass.startofReceipt();
+				System.out.println(transaction);
+				outclass.receiptTotal(totalfee);
+				outclass.endofReceipt();
+				exitproceed = outclass.exitorProceed();
+				transaction = "";
+				if (exitproceed == 2) {
+					break;
+				} 
+			} 
+			
+		}
 
 	}
 
--- ParkTicketing/OutputClass.java
+++ ParkTicketing/OutputClass.java
@@ -19,9 +19,16 @@ public class OutputClass {
 	
 	public int nextRound() {
 		int nextRound;
-		System.out.printf("계속 진행(1: 새로운 주문, 2: 프로그램 종료) : ");
+		System.out.printf("계속 발권하시겠습니까?\n1: 티켓 발권\n2: 종료\n");
 		nextRound = scan.nextInt();
 		return nextRound;
+	}
+
+	public int exitorProceed() {
+		int exitproceed;
+		System.out.printf("계속 진행(1: 새로운 주문, 2: 프로그램 종료) : ");
+		exitproceed = scan.nextInt();
+		return exitproceed;
 	}
 	
 	public String ageType(int age) {
@@ -38,5 +45,45 @@ public class OutputClass {
 			type = "유아(무료)";
 		}
 		return type;
+	}
+	
+	public void startofReceipt() {
+		System.out.println("================== 폴리랜드 ==================");
+	}
+	
+	public String receipt(int type, String ageType, int numTicket, int fee, int prefType) {
+		String ticketType = "";
+		if (type == 1) {
+			ticketType = "주간권";
+		} else if (type == 2) {
+			ticketType = "야간권";
+		}
+		
+		String pref = "";
+		if (prefType == 1) {
+			pref = "우대적용 없음";
+		} else if (prefType == 2) {
+			pref = "장애인 우대적용";
+		} else if (prefType == 3) {
+			pref = "국가유공자 우대적용";
+		} else if (prefType == 4) {
+			pref = "다자녀 우대적용";
+		} else if (prefType == 5) {
+			pref = "임산부 우대적용";
+		}
+		
+		String transaction = String.format("%s %s X %d% d원 *%s\n", 
+				ticketType, ageType, numTicket, fee, pref);
+		
+		return transaction;
+	}
+	
+	public void receiptTotal(int totalFee) {
+		System.out.println();
+		System.out.printf("입장료 총액은 %d원 입니다\n", totalFee);
+	}
+	
+	public void endofReceipt() {
+		System.out.println("==============================================\n");
 	}
 }
